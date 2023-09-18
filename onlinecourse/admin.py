96b38626a617be9129bac97a3bfca0f2c1662c50
alilefta <alilefta95@gmail.com>
2023-09-18 17:15:47 +0300

    creating and registering models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -22,9 +22,43 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'grade', 'get_choices_display']
+    # list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    def get_choices_display(self, obj):
+        choices = obj.choice_set.all()
+        choice_texts = ", ".join([choice.choice_text for choice in choices])
+        return choice_texts
+
+    get_choices_display.short_description = 'Choices'
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text', 'is_correct', 'question_text']
+    # list_filter = ['pub_date']
+    search_fields = ['choice_text']
+
+    def question_text(self, obj):
+        return obj.question.question_text
 # <HINT> Register Question and Choice models here
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
